1bcd2d2ea682520ecba44ed9fde09809c87b2544
Taewoo Kim <xodn4195@gmail.com>
2017-07-01 00:21:08 +0900

    Add rank in freezeRankInfo

--- httpserver/server.go
+++ httpserver/server.go
@@ -34,6 +34,8 @@ func StartServer(rankInfo *rank.RankInfo, rankInfoFreeze *rank.RankInfo, port ui
     }
 
     r := rankInfo.GetUserSummary(uint(userId))
+    r.Rank = rankInfoFreeze.GetUserSummary(uint(userId)).Rank
+
     if r == nil {
       return ctx.NoContent(http.StatusNotFound)
     }
