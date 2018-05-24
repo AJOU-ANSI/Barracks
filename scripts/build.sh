7a45768b0db332acd320cbc3acd06bf4296bc2d5
Taewoo Kim <xodn4195@gmail.com>
2018-05-24 12:32:03 +0900

    Add os env get

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-printenv
 dep version || curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
 dep ensure
+cp data/config.go.sample data/config.go
 go build main.go -o main
